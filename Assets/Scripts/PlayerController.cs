d9ad19ed5a4e14b0b00e1158b0e77d93dce699fb
Hamad Alaslani <hamadsmma@gmail.com>
2025-06-30 19:45:25 +0300

    Add toggle between fps and tps camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,13 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public GameObject TPSCamera;
+    public GameObject FPSCamera;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        FPSCamera.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,6 +20,12 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TPSCamera.SetActive(!TPSCamera.activeSelf);
+            FPSCamera.SetActive(!FPSCamera.activeSelf);
+        }
         //Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
